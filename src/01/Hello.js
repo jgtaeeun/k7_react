ef667db9749abf33a41418992a70292671631a05
jgtaeeun <jas49258667@gmail.com>
2024-05-02 17:56:55 +0900

    react1

---
+++ src/01/Hello.js
@@ -0,0 +1,16 @@
+function Hello(){
+    return(
+        <>
+         <p>
+          첫번째 리액트 : 함수형 컴포넌트
+         </p>
+
+         <ul>
+          <li>Hello.js 만들기</li>
+         </ul>
+
+        </>
+    );
+}
+
+export default Hello;
